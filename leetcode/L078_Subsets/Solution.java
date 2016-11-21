b978f93def04d7e3a67a09745f49208fdc9dda6b
deadEnding <deadend.endif@gmail.com>
2016-11-21 23:19:27 +0800

    day 5: 5

--- leetcode/L078_Subsets/Solution.java
+++ leetcode/L078_Subsets/Solution.java
@@ -34,12 +34,32 @@ public class Solution {
 
     public static void main(String[] args) {
         int[] nums = {1,2,3};
-        List<List<Integer>> result = new Solution().subsets(nums);
+        List<List<Integer>> result = new Solution2().subsets(nums);
         for (List<Integer> list : result) {
             for (int i : list) {
                 System.out.print(i + " ");
             }
             System.out.println();
         }
+    }
+}
+
+class Solution2 {
+
+    private List<List<Integer>> result = new LinkedList<>();
+
+    private void dfs(int[] nums, int ix, LinkedList<Integer> path) {
+        result.add(new LinkedList<Integer>(path));
+
+        for (int i = ix; i < nums.length; i++) {
+            path.addLast(nums[i]);
+            dfs(nums, i + 1, path);
+            path.removeLast();
+        }
+    }
+
+    public List<List<Integer>> subsets(int[] nums) {
+        dfs(nums, 0, new LinkedList<Integer>());
+        return result;
     }
 }
