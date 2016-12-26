254e78620515e14d9bfa4c859508c841b42a6dd0
deadEnding <deadend.endif@gmail.com>
2016-12-26 10:49:05 +0800

    optimization

--- leetcode/L005_LongestPalindromicSubstring/Solution.java
+++ leetcode/L005_LongestPalindromicSubstring/Solution.java
@@ -7,7 +7,58 @@ package leetcode.L005_LongestPalindromicSubstring;
  * @description:
  */
 
+
+// Manacher算法
 public class Solution {
+    private String insert(String s, char ch) {
+        StringBuffer sb = new StringBuffer();
+
+        for (char c : s.toCharArray()) {
+            sb.append('#');
+            sb.append(c);
+        }
+        sb.append('#');
+
+        return sb.toString();
+    }
+
+    public String longestPalindrome(String s) {
+        String ns = insert(s, '#');
+        int[] p = new int[ns.length()];
+
+        int maxi = 0, max = 0;
+        int id = 0, mx = 1;
+        for (int i = 1; i < ns.length(); i++) {
+            // 关键点
+            p[i] = mx > i ? Math.min(p[2 * id - i], mx - i) : 1;
+            // 向左右延伸求解
+            while (i - p[i] >= 0 && i + p[i] < ns.length() && ns.charAt(i + p[i]) == ns.charAt(i - p[i])) {
+                p[i]++;
+            }
+            // 更新右边界最大值
+            if (i + p[i] > mx) {
+                mx = i + p[i];
+                id = i;
+            }
+            // 更新最大值
+            if (p[i] - 1 > max) {
+                max = p[i] - 1;
+                maxi = i;
+            }
+        }
+        return s.substring((maxi - max + 1) / 2, (maxi + max) / 2);
+    }
+
+
+    public static void main(String[] args) {
+        String s = "baaaa";
+        System.out.println(new Solution().longestPalindrome(s));
+    }
+}
+
+
+// 动态规划
+class DPSolution {
     public String longestPalindrome(String s) {
         final int n = s.length();
         boolean[][] isPali = new boolean[n][n];
@@ -23,11 +74,6 @@ public class Solution {
             }
         }
         return s.substring(start, start + maxLen);
-    }
-
-    public static void main(String[] args) {
-        String s = "aaaa";
-        System.out.println(new Solution().longestPalindrome(s));
     }
 }
 
